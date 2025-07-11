6b7b2aa9567723135de0c67718f1e83d4921299a
xxuxa-k <xyz@tk-tech.xyz>
2025-07-11 19:34:31 +0900

    tmp commit

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,25 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { resolve } from 'path';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const fn = new lambda.Function(this, 'MyFunction', {
+    const fn = new NodejsFunction(this, "MyFunction", {
+      entry: resolve(__dirname, "../../lambda-ts/", "001_sample", "index.ts"),
       runtime: lambda.Runtime.NODEJS_22_X,
-      code: lambda.Code.fromAsset(resolve("../../lambda-ts/001_sample")),
-      handler: "index.handler",
+      handler: "handler",
+      bundling: {
+        minify: true,
+        sourceMap: true,
+        target: "es2020",
+      }
     })
-
-    const api = new apigateway.LambdaRestApi(this, "HelloWorldApi", {
-      handler: fn,
-      proxy: false,
-    })
-
-    const helloResource = api.root.addResource("hello")
-    helloResource.addMethod("GET")
   }
 }
